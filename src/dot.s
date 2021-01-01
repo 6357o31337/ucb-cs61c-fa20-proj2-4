87a11afdf7c54e1813df958676b516f3001bd5fc
6357o31337 <68189875+6357o31337@users.noreply.github.com>
2021-01-01 14:43:47 +0900

    Update dot.s

--- src/dot.s
+++ src/dot.s
@@ -19,24 +19,48 @@
 # =======================================================
 dot:
 
+    ebreak
+
     # Prologue
+    
+    
+    
+    mv t0 zero
+    
+    mv t4 zero
+
+    # li t5 4
+    mv t5 zero
+    addi t5, t5, 4
+
+    mul a3, a3, t5
+    mul a4, a4, t5
 
 
 loop_start:
+    # we are done once t0 >= a2
+    bge t0, a2, loop_end
+    
+    # t1 := *a0
+    lw t1, 0(a0)
+    lw t2, 0(a1)
+    
+    #
+    mul t3, t1, t2
 
-
-
-
-
-
-
-
+    add t4, t4, t3
+    
+    # go to next value
+    addi t0, t0, 1
+    add a0, a0, a3
+    add a1, a1, a4
+    j loop_start
 
 
 
 
 loop_end:
-
+    mv a0, t4
 
     # Epilogue
 
